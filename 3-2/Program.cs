b897b4955205674cd5ef502d0b855b5037a628f5
AlexDesmos <154222154+AlexDesmos@users.noreply.github.com>
2024-02-02 16:52:11 +0300

    done 3-2

--- 3-2/Program.cs
+++ 3-2/Program.cs
@@ -16,6 +16,26 @@ namespace _3_2
                 int length = int.Parse(Console.ReadLine());
                 One_array one_array = new One_array(person, length);
             }
+            else if (input == "two_array")
+            {
+                Console.WriteLine("Write length of your array:");
+                int length = int.Parse(Console.ReadLine());
+                Console.WriteLine("Write width of your array:");
+                int width = int.Parse(Console.ReadLine());
+
+                Two_array two_array = new Two_array(person, length, width);
+
+            }
+            else if (input == "Step_dimensional")
+            {
+                Console.WriteLine("Write width of your array:");
+                int width = int.Parse(Console.ReadLine());
+
+
+                Step_array step_array = new Step_array(person, width);
+
+            }
+
         }
     }
 }
---
+++ 3-2/Step_array.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_2
+{
+    class Step_array : Dad_class
+    {
+        Random rnd = new Random();
+        private int[][] array;
+        public Step_array(bool person, int width)
+        {
+            array = new int[width][];
+            if (person)
+                userFill();
+            else
+                rndFill();
+            print();
+            getmid();
+        }
+        public override void userFill()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Write length of the line:");
+                int length = int.Parse(Console.ReadLine());
+                array[i] = new int[length];
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    Console.WriteLine($"X: {j} Y: {i}");
+                    int num = int.Parse(Console.ReadLine());
+                    array[i][j] = num;
+                }
+            }
+        }
+        public override void rndFill()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                array[i] = new int[rnd.Next(1, 5)];
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    array[i][j] = rnd.Next(-100, 100);
+                }
+            }
+        }
+        public override void print()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+
+                    Console.Write(array[i][j] + "\t");
+                }
+            }
+        }
+        public override void getmid()
+        {
+            int mid_num = 0;
+            int array_count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    array_count += 1;
+                    mid_num += array[i][j];
+                }
+            }
+            Console.WriteLine($"midnum of the array: {mid_num / array_count}");
+        }
+    } 
+}
